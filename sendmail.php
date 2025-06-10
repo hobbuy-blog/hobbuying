fd7ffe7bcc34f26f7074a5f3395f27bff9588fc6
bockring <162660957+bockring@users.noreply.github.com>
2025-06-10 12:19:06 +0900

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "your@email.com"; // ←あなたのメールアドレス
-    $subject = "お問い合わせフォーム";
+    $to = "bockring.scratcher@gmail.com"; // ←あなたのメールアドレス
+    $subject = "GitHub Pagesお問い合わせ";
     $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
     $email = htmlspecialchars($_POST["email"], ENT_QUOTES, 'UTF-8');
     $message = htmlspecialchars($_POST["message"], ENT_QUOTES, 'UTF-8');
 
-    $body = "お名前: $name\nメールアドレス: $email\n\nメッセージ:\n$message";
+    $body = "==重要通知==\n\nGitHub Pagesで作成されたページの閲覧者から問い合わせが届いています。\n早急に対応してください。\n\n==問い合わせ者概要==\n お名前 : $name\nメールアドレス : $email\n\n==問い合わせ内容==\n$message";
     $headers = "From: $email\r\nReply-To: $email\r\n";
 
     if (mail($to, $subject, $body, $headers)) {
